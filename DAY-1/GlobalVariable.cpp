0acbdc31c35e2ee7593a063db73a47f07ee4aeaa
PrachiSharma1002 <prachidilipsharma@gmail.com>
2026-05-04 13:06:29 +0530

    normal commit

---
+++ DAY-1/GlobalVariable.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int gv;
+int main(){
+    gv=200;
+    int x;
+    cout<<"enter value of x:";
+    cin>>x;
+    double result =(double)gv/x;
+    cout<<"result:"<<result;
+    return 0;
+}
