7fb6172af37c7fda89ccf79ce68313b87c4f36a7
PrachiSharma1002 <prachidilipsharma@gmail.com>
2026-05-04 13:12:25 +0530

    normal commit

---
+++ DAY-1/BitwiseMultiDiv.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+     int a,b;
+     cout<<"enter value of a,b :";
+     cin>>a>>b;
+     a=a<<2;
+     b=b>>2;
+     cout<<"after multiplication:"<<a<<endl;
+     cout<<"after division:"<<b;
+        return 0;
+}
