381518026b7020f200caffbba10add49ec3a121d
PrachiSharma1002 <prachidilipsharma@gmail.com>
2026-05-04 09:36:48 +0530

    Added c++ code

---
+++ DAY-1/temperature.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ float c,f;
+    cout<<"Enter temperature in celsius:";
+    cin>>c;
+    
+    f=(9.0/5.0)*c+32;
+    cout<< "Temperature in fahrenheit:"<<f<<endl;
+
+   return 0;
+ }
