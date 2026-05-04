5e3e0a5cce08c700c433c4a517899d055132d2c4
PrachiSharma1002 <prachidilipsharma@gmail.com>
2026-05-04 13:19:30 +0530

    normal commit

---
+++ DAY-1/SumAvg.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,sum,avg;
+    cout<<"Enter value of a, b and c:";
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    avg=sum/3;
+    cout<<"Sum is:"<<sum<<endl;
+    cout<<"Avg is:"<<avg;
+return 0;
+}
